462d58fe82285e8e7e7724caa88cbbe44b6f0679
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-06-14 21:18:14 -0300

    add hero component

---
+++ frontend/src/components/Hero/index.js
@@ -0,0 +1,13 @@
+import { Image } from '@chakra-ui/react';
+
+const Hero = () => {
+  return (
+    <Image
+      width="100vw"
+      src="https://www.themoviedb.org/t/p/original/zvQgzyelcgSYNr4GpPXEEgl1i7O.jpg"
+      alt="Kiki"
+    />
+  );
+};
+
+export default Hero;
