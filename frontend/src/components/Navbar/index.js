acbf7db8a89a1533f2d126e936289e8ad635e0aa
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-06-15 19:32:52 -0300

    add navbar

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,4 +1,13 @@
-import { Flex, Image, Text, HStack } from '@chakra-ui/react';
+import {
+  Flex,
+  Image,
+  HStack,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Link,
+} from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
 import Logo from '../../assets/Logo.svg';
 
 const routes = [
@@ -16,11 +25,17 @@ const routes = [
   },
 ];
 
-const Navbar = () => {
+// homepage -> navbar gradiente
+// todos os outros lugares -> navbar com background
+
+const Navbar = ({ transparent = false }) => {
+  const background = transparent
+    ? 'linear(to-b,rgba(0,0,0,0.3),transparent)'
+    : 'linear(to-r,m180.navyBlue.500,m180.navyBlue.400)';
   return (
     <Flex
       color="white"
-      bg="m180.navyBlue.500"
+      bgGradient={background}
       width="100%"
       height="6rem"
       justifyContent="space-between"
@@ -32,8 +47,17 @@ const Navbar = () => {
 
       <HStack spacing="24px">
         {routes.map(route => (
-          <Text key={route.route}>{route.title}</Text>
+          <Link minWidth="7rem" key={route.route} href={route.route}>
+            {route.title}
+          </Link>
         ))}
+        <InputGroup>
+          <InputRightElement
+            pointerEvents="none"
+            children={<SearchIcon color="m180.purple" />}
+          />
+          <Input variant="filled" type="search" placeholder="Search" />
+        </InputGroup>
       </HStack>
     </Flex>
   );
