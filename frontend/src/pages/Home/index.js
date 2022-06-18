094f34fbcc2c5d7500d218eee16524e6f36bd38c
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-06-18 01:08:41 -0300

    add initial homepage structure

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,19 @@
-import { Text } from '@chakra-ui/react';
 import Hero from '../../components/Hero';
+import HomeGrid from '../../components/Movies/HomeGrid/HomeGrid';
+import BasicGrid from '../../components/Grid/BasicGrid';
+import { Button, Heading, Box } from '@chakra-ui/react';
 
 const Home = () => {
   return (
-    <Hero src="https://www.themoviedb.org/t/p/original/zvQgzyelcgSYNr4GpPXEEgl1i7O.jpg" />
+    <Box>
+      <Box>
+        <Hero src="https://www.themoviedb.org/t/p/original/zvQgzyelcgSYNr4GpPXEEgl1i7O.jpg" />
+      </Box>
+      <Box paddingLeft="15rem" paddingRight="15rem">
+        <HomeGrid></HomeGrid>
+        <BasicGrid></BasicGrid>
+      </Box>
+    </Box>
   );
 };
 
