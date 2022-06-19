722c14ff9eb4c0dec94535e7e910f5399be12901
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-06-18 21:10:52 -0300

    add popular movies row with mocked data

---
+++ frontend/src/components/PopularMoviesRow/index.js
@@ -0,0 +1,22 @@
+import { Link, Image, Box, HStack } from '@chakra-ui/react';
+
+const PopularMoviesRow = ({ data = [] }) => {
+  const modifiedData = data.slice(0, 6);
+  const mediaUrl = process.env.REACT_APP_TMDB_MEDIA_URL;
+  return (
+    <HStack spacing="24px" paddingTop="5rem" paddingBottom="5rem">
+      {modifiedData.map(movie => (
+        <Box key={movie.id} flex="1">
+          <Link href={`/movie/${movie.id}`}>
+            <Image
+              src={mediaUrl + 'w300' + movie.poster_path}
+              alt={movie.title}
+            />
+          </Link>
+        </Box>
+      ))}
+    </HStack>
+  );
+};
+
+export default PopularMoviesRow;
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,10 +1,67 @@
 import Hero from '../../components/Hero';
-import HomeGrid from '../../components/Movies/HomeGrid';
+import PopularMoviesRow from '../../components/PopularMoviesRow';
 import BasicGrid from '../../components/Grid';
 import { Box } from '@chakra-ui/react';
 import ReviewGrid from '../../components/Review';
-import { useEffect, useState } from 'react';
 
+const mockPopularMoviesRow = [
+  {
+    id: 629542,
+    title: 'The Bad Guys',
+    genre_ids: [16, 35, 10751, 80],
+    release_date: '2022-03-17',
+    poster_path: '/7qop80YfuO0BwJa1uXk1DXUUEwv.jpg',
+    backdrop_path: '/7qop80YfuO0BwJa1uXk1DXUUEwv.jpg',
+  },
+  {
+    id: 916821,
+    title: 'Gasoline Alley',
+    genre_ids: [28, 53, 80, 9648],
+    release_date: '2022-02-25',
+    poster_path: '/ews3l3v7JYLtBS5ansZrXsXLMzl.jpg',
+    backdrop_path: '/ews3l3v7JYLtBS5ansZrXsXLMzl.jpg',
+  },
+  {
+    id: 628900,
+    title: 'The Contractor',
+    genre_ids: [28, 53],
+    release_date: '2022-03-10',
+    poster_path: '/rJPGPZ5soaG27MK90oKpioSiJE2.jpg',
+    backdrop_path: '/rJPGPZ5soaG27MK90oKpioSiJE2.jpg',
+  },
+  {
+    id: 756681,
+    title: 'Veneciafrenia',
+    genre_ids: [27],
+    release_date: '2022-04-21',
+    poster_path: '/luMC56bwZqaECYRz6X7sXjqN6nd.jpg',
+    backdrop_path: '/luMC56bwZqaECYRz6X7sXjqN6nd.jpg',
+  },
+  {
+    id: 785985,
+    title: 'The Takedown',
+    genre_ids: [35, 28],
+    release_date: '2022-05-06',
+    poster_path: '/h5hVeCfYSb8gIO0F41gqidtb0AI.jpg',
+    backdrop_path: '/h5hVeCfYSb8gIO0F41gqidtb0AI.jpg',
+  },
+  {
+    id: 406759,
+    title: 'Moonfall',
+    genre_ids: [878, 12, 28],
+    release_date: '2022-02-03',
+    poster_path: '/putDqnndrdRzRRy5sVPYMH5FTjI.jpg',
+    backdrop_path: '/putDqnndrdRzRRy5sVPYMH5FTjI.jpg',
+  },
+  {
+    id: 568124,
+    title: 'Encanto',
+    genre_ids: [16, 35, 10751, 14],
+    release_date: '2021-11-24',
+    poster_path: '/4j0PNHkMr5ax3IA8tjtxcmPU3QT.jpg',
+    backdrop_path: '/4j0PNHkMr5ax3IA8tjtxcmPU3QT.jpg',
+  },
+];
 const Home = () => {
   return (
     <Box>
@@ -12,9 +69,9 @@ const Home = () => {
         <Hero src="https://www.themoviedb.org/t/p/original/zvQgzyelcgSYNr4GpPXEEgl1i7O.jpg" />
       </Box>
       <Box paddingLeft="15rem" paddingRight="15rem">
-        <HomeGrid></HomeGrid>
-        <BasicGrid></BasicGrid>
-        <ReviewGrid></ReviewGrid>
+        <PopularMoviesRow data={mockPopularMoviesRow} />
+        <BasicGrid />
+        <ReviewGrid />
       </Box>
     </Box>
   );
