4d47821d5c4263e947321fd4e00fa0dd53cd548d
Gabriel Freire <gabrielfvale@gmail.com>
2022-06-19 22:13:20 -0300

    Update ListCard with stacked posters

---
+++ frontend/src/components/ListCard/StackedPosters/index.js
@@ -0,0 +1,50 @@
+import { useRef } from 'react';
+import { Image, Box, useDimensions } from '@chakra-ui/react';
+
+const StackedPosters = ({ posters = [] }) => {
+  const elementRef = useRef();
+  const dimensions = useDimensions(elementRef);
+
+  const mediaUrl = process.env.REACT_APP_TMDB_MEDIA_URL;
+  const w = 'w300';
+
+  const maxImages = posters.length < 4 ? posters.length : 4;
+  const iterableImages = posters.slice(0, maxImages);
+  const imageWidth = 100;
+  const imageAspectRatio = 300 / 445;
+  const containerHeight = imageWidth / imageAspectRatio;
+
+  const calcOffset = pos => {
+    if (!dimensions) return 0;
+
+    const width = dimensions.contentBox.width;
+    const maxOffset = width - imageWidth;
+    const offset = maxOffset / (maxImages - 1);
+
+    return pos * offset;
+  };
+
+  return (
+    <Box
+      ref={elementRef}
+      borderRadius="0.4rem"
+      position="relative"
+      height={containerHeight}
+      overflow="hidden"
+    >
+      {iterableImages.map((src, index) => (
+        <Image
+          key={src + index}
+          src={`${mediaUrl}${w}${src}`}
+          width="100px"
+          borderRadius="0.4rem"
+          position="absolute"
+          top="0"
+          left={() => calcOffset(index)}
+        />
+      ))}
+    </Box>
+  );
+};
+
+export default StackedPosters;
--- frontend/src/components/ListCard/index.js
+++ frontend/src/components/ListCard/index.js
@@ -1,26 +1,25 @@
-import { Box, Image, Text } from '@chakra-ui/react';
+import { Box, HStack, Text, useTheme } from '@chakra-ui/react';
 import { AiFillHeart } from 'react-icons/ai';
+import StackedPosters from './StackedPosters';
 
-const ListCard = ({ imageUrl, title, likes }) => {
+const ListCard = ({ title = '', movies = [], likes = 0 }) => {
+  const theme = useTheme();
+  const moviePosters = movies.map(movie => movie.poster_path);
   return (
     <Box
-      flex={1}
       backgroundColor="m180.darkBeige"
-      marginX="1rem"
       padding="0.5rem"
       borderRadius="0.4rem"
+      width="20rem"
     >
-      <Image
-        src={imageUrl}
-        borderRadius="0.4rem"
-        width="100%"
-        height="10rem"
-        objectFit={'cover'}
-      />
-      <Text fontWeight={'medium'}>{title}</Text>
-      <Text fontSize={'xs'} fontWeight={'medium'}>
-        <AiFillHeart /> {likes} likes
-      </Text>
+      <StackedPosters posters={moviePosters} />
+      <Text fontWeight="medium">{title}</Text>
+      <HStack>
+        <AiFillHeart color={theme.colors.m180.pink['500']} />
+        <Text fontSize="xs" fontWeight="medium">
+          {likes} likes
+        </Text>
+      </HStack>
     </Box>
   );
 };
