b1ca73ee89afd500d2ae5a859d47690122ee11ed
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-06-24 23:15:36 -0300

    add comment

--- frontend/src/pages/Lists/index.js
+++ frontend/src/pages/Lists/index.js
@@ -3,6 +3,7 @@ import ListComment from '../../components/ListComment';
 
 const Lists = () => {
   return (
+    // Testing ListComment component
     <Box paddingLeft="15rem" paddingRight="15rem">
       <ListComment username="Ola" />
     </Box>
