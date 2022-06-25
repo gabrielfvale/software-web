1824e72579915620f4eece30ffcf950f466f64ae
JPortela4c <jportela4c@gmail.com>
2022-06-25 16:36:00 +0100

    add reviews to profile page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,8 @@
 import { Box, Flex } from '@chakra-ui/react';
 import ProfileHeader from './components/ProfileHeader';
 import ListCard from '../../components/ListCard';
+import ReviewList from '../../components/ReviewList';
+import Category from '../../components/Category';
 
 const mockLists = {
   results: [
@@ -61,6 +63,39 @@ const mockLists = {
   ],
 };
 
+const mockReviews = [
+  {
+    review_id: '1',
+    user_id: '1',
+    movie_api_id: '526896',
+    score: '2.5',
+    description: 'The most move of all time',
+    created_at: '2022-06-09T03:27:33.143Z',
+    updated_at: '2022-06-09T00:27:57.936Z',
+    likes: '0',
+    comments: '1',
+    username: 'laurazona4',
+    title: 'Morbius',
+    release_date: '2022-03-30',
+    poster_path: '/6JjfSchsU6daXk2AKX8EEBjO3Fm.jpg',
+  },
+  {
+    review_id: '2',
+    user_id: '2',
+    movie_api_id: '526896',
+    score: '5.0',
+    description: 'The most move of all time',
+    created_at: '2022-06-09T03:27:33.143Z',
+    updated_at: '2022-06-09T00:27:57.936Z',
+    likes: '0',
+    comments: '1',
+    username: 'laurazona4',
+    title: 'Morbius',
+    release_date: '2022-03-30',
+    poster_path: '/6JjfSchsU6daXk2AKX8EEBjO3Fm.jpg',
+  },
+];
+
 const mockStats = {
   movies_reviewed: 0,
   lists_created: 0,
@@ -82,8 +117,7 @@ const Profile = () => {
   return (
     <Box marginX="15rem" paddingY="1.5rem">
       <ProfileHeader user={mockUser} />
-      <Box marginBottom="1.5rem" />
-      <Flex gap="5">
+      <Flex gap="5" marginY="1.5rem">
         {lists.map(({ list_id, title, movies, likes }, index) => (
           <ListCard
             key={title + index}
@@ -94,6 +128,9 @@ const Profile = () => {
           />
         ))}
       </Flex>
+      <Category text="Recent Reviews">
+        <ReviewList data={mockReviews} />
+      </Category>
     </Box>
   );
 };
