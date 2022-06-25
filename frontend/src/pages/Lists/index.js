4fdb45f11c2e36e205977e26768285a82cd0ec74
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-06-24 23:14:32 -0300

    add ListComment

--- frontend/src/pages/Lists/index.js
+++ frontend/src/pages/Lists/index.js
@@ -1,5 +1,12 @@
+import { Box } from '@chakra-ui/react';
+import ListComment from '../../components/ListComment';
+
 const Lists = () => {
-  return <></>;
+  return (
+    <Box paddingLeft="15rem" paddingRight="15rem">
+      <ListComment username="Ola" />
+    </Box>
+  );
 };
 
 export default Lists;
