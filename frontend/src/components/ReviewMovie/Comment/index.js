4301caa1b65f7611ab88f93e6db2651bc1f191e0
Laura Petrola <46495368+petrolau@users.noreply.github.com>
2022-06-24 22:12:16 -0300

    add comment (reply review) component.

---
+++ frontend/src/components/ReviewMovie/Comment/index.js
@@ -0,0 +1,31 @@
+import { Textarea, Button, VStack, Text, HStack } from '@chakra-ui/react';
+
+const Comment = ({ username = '', description = '', onChange = () => {} }) => {
+  return (
+    <VStack width="100%" alignItems="flex-start">
+      <Text as="span" fontSize="xs">
+        Commenting as{' '}
+        <Text as="span" fontSize="xs" fontWeight="bold">
+          {username}
+        </Text>
+      </Text>
+      <Textarea
+        maxlength="280"
+        bg="white"
+        fontSize="xs"
+        value={description}
+        onChange={onChange}
+      />
+      <HStack
+        alignItems="flex-start"
+        justifyContent="space-between"
+        width="100%"
+      >
+        <Text fontSize="xs">{description.length}/280</Text>
+        <Button size="md">Send</Button>
+      </HStack>
+    </VStack>
+  );
+};
+
+export default Comment;
