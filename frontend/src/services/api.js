5a60bdccf0c53f9cfa21d89f7b531741895c14a9
Gabriel Freire <gabrielfvale@gmail.com>
2022-06-27 17:47:14 -0300

    Add simple authentication service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
+import { getToken } from './auth';
 
 const baseURL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL,
+});
+
+api.interceptors.request.use(async config => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
